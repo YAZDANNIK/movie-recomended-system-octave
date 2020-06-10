5b49a4bba48cf60c4109033e03dac797c62912db
Witch-kinG <samanyazdannik@gmail.com>
2020-06-10 23:05:55 +0430

    first commit to movie recomender system with octave

---
+++ estimateGaussian.m
@@ -0,0 +1,15 @@
+function [mu sigma2] = estimateGaussian(X)
+
+
+[m, n] = size(X);
+
+mu = zeros(n, 1);
+sigma2 = zeros(n, 1);
+
+mu = 1/m * sum(X);
+
+sigma2 = 1/m * sum((X - repmat(mu, m, 1)).^2);
+
+
+
+end
---
+++ loadMovieList.m
@@ -0,0 +1,19 @@
+function movieList = loadMovieList()
+y of the words in movieList.
+
+
+
+fid = fopen('movie_ids.txt');
+
+
+n = 1682;  
+
+movieList = cell(n, 1);
+for i = 1:n
+    line = fgets(fid);
+    [idx, movieName] = strtok(line, ' ');
+    movieList{i} = strtrim(movieName);
+end
+fclose(fid);
+
+end
